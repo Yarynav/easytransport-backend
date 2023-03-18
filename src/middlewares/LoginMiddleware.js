f5e664abf4faf85cd3b671cd70a1af3e7d96ec2d
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2023-03-18 11:03:08 -0300

    feat: Modifications are made to the mvc structure

--- src/middlewares/LoginMiddleware.js
+++ src/middlewares/LoginMiddleware.js
@@ -1,21 +1,22 @@
-const { validateToken } = require('../helpers/validateToken');
-const { showError } = require('../helpers/errorHelper');
+const { validateToken } = require('../helpers/validateToken')
+const { showError } = require('../helpers/errorHelper')
 
 const loginMiddleware = async (req, res, next) => {
+  console.log('Hola:', req)
   try {
     if (!req.header('Authorization')) {
-      return res.status(401).send({ message: 'Por favor inicia sesion' });
+      return res.status(401).send({ message: 'Por favor inicia sesion' })
     }
-    const token = req.header('Authorization').split(' ')[1];
-    const tokenData = await validateToken(token, res);
+    const token = req.header('Authorization').split(' ')[1]
+    const tokenData = await validateToken(token, res)
     if (!tokenData) {
-      return res.status(401).send({ message: 'Token invalido' });
+      return res.status(401).send({ message: 'Token invalido' })
     }
-    req.user = tokenData;
-    next();
+    req.user = tokenData
+    next()
   } catch (e) {
-    return showError(res, e);
+    return showError(res, e)
   }
-};
+}
 
-module.exports = { loginMiddleware };
+module.exports = { loginMiddleware }
--- src/routes/ClientRoutes.js
+++ src/routes/ClientRoutes.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const ClientController = require('../controllers/ClientController');
-const { loggerMiddleware } = require('../middlewares/LoggerMiddleware');
-const { loginMiddleware } = require('../middlewares/LoginMiddleware');
-const middlewares = [loggerMiddleware, loginMiddleware];
+const express = require('express')
+const router = express.Router()
+const ClientController = require('../controllers/ClientController')
+const { loggerMiddleware } = require('../middlewares/LoggerMiddleware')
+const { loginMiddleware } = require('../middlewares/LoginMiddleware')
+const middlewares = [loggerMiddleware, loginMiddleware]
 
 /** @description Iniciar sesión como un cliente  */
-router.post('/login', loggerMiddleware, ClientController.login);
+router.post('/login', loggerMiddleware, ClientController.login)
 
 /** @description Crear una cuenta para cliente  */
-router.post('/signin', loggerMiddleware, ClientController.signin);
+router.post('/signin', loggerMiddleware, ClientController.signin)
 
 /** @description Listado de todos los clientes */
-router.get('/', middlewares, ClientController.list);
+router.get('/', middlewares, ClientController.list)
 
 /** @description Obtener información de un cliente específico a través de su id */
-router.get('/:id', middlewares, ClientController.getById);
+router.get('/:id', middlewares, ClientController.getById)
 
 /** @description Eliminar de manera lógica un cliente */
-router.delete('/:id', middlewares, ClientController.remove);
+router.delete('/:id', middlewares, ClientController.remove)
 
 /** @description Modificar la información de un cliente */
-router.put('/:id', middlewares, ClientController.update);
+router.put('/:id', middlewares, ClientController.update)
 
-module.exports = router;
+module.exports = router
