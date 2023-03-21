4170cb5c2011fd92102ad48f6aa32ace1360efcf
yary navarrete <yar.navarrete@gmail.com>
2023-03-20 22:53:13 -0300

    ADD: Shipping router, controller and model

---
+++ src/routes/ShippingRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const ShippingController = require('../controllers/ShippingController');
+const { loggerMiddleware } = require('../middlewares/LoggerMiddleware');
+const { loginMiddleware } = require('../middlewares/LoginMiddleware');
+const middlewares = [loggerMiddleware, loginMiddleware];
+
+/** @description Obtiene todos los viajes relacionados a un cliente  */
+router.get('/byClient', middlewares, ShippingController.getByClientId);
+
+router.put('/:id', middlewares, ShippingController.update);
+
+module.exports = router;
