f7b3e4932ee315f0639b8b7847b5dfdbf30238c7
yarnav <yar.navarrete@gmail.com>
2023-03-27 20:03:38 -0300

    ADD: create shipping view

--- src/routes/ShippingRoutes.js
+++ src/routes/ShippingRoutes.js
@@ -4,12 +4,13 @@ const ShippingController = require('../controllers/ShippingController');
 const { loggerMiddleware } = require('../middlewares/LoggerMiddleware');
 const { loginMiddleware } = require('../middlewares/LoginMiddleware');
 const { shippingMiddleware } = require('../middlewares/ShippingMiddleware');
-const middlewares = [loggerMiddleware, loginMiddleware, shippingMiddleware];
+const middlewares = [loggerMiddleware, loginMiddleware];
+const createMiddlewares = [...middlewares, shippingMiddleware];
 
 /** @description Obtiene todos los viajes relacionados a un cliente  */
 router.get('/byClient', middlewares, ShippingController.getByClientId);
 router.put('/:id', middlewares, ShippingController.update);
 router.get('/:id', middlewares, ShippingController.getById);
-router.post('/', middlewares, ShippingController.create);
+router.post('/', createMiddlewares, ShippingController.create);
 
 module.exports = router;
