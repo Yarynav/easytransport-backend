8cb902d219a62674f55b4d48c5e8b7fd330cb030
yarnav <yar.navarrete@gmail.com>
2023-03-28 22:20:56 -0300

    ADD: client and transport validation

--- src/controllers/TransportController.js
+++ src/controllers/TransportController.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const TransportModel = require('../models/TransportModel')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const TransportModel = require('../models/TransportModel');
 
 const getTokenBody = (transport) => {
   return {
@@ -16,107 +16,141 @@ const getTokenBody = (transport) => {
     total_starts: transport.total_starts,
     num_qualification: transport.num_qualification,
     role: 'transport',
-  }
-}
+  };
+};
 
 const login = async (req, res) => {
-  let { email, password } = req.body
-  const transport = await TransportModel.getByEmail(email)
-  const passwordIsCorrect = bcrypt.compareSync(password, transport.password)
+  try {
+    let { email, password } = req.body;
+    const transport = await TransportModel.getByEmail(email);
+    const passwordIsCorrect = bcrypt.compareSync(password, transport.password);
 
-  if (passwordIsCorrect && transport) {
-    const tokenPayload = getTokenBody(transport)
-    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET)
-    res.send(token)
-  } else {
-    res.status(401).json({ message: 'Email o contraseña incorrecta' })
+    if (passwordIsCorrect && transport) {
+      const tokenPayload = getTokenBody(transport);
+      const token = jwt.sign(tokenPayload, process.env.JWT_SECRET);
+      res.send(token);
+    } else {
+      res.status(401).json({ message: 'Email o contraseña incorrecta' });
+    }
+  } catch (e) {
+    console.log('falla', error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
   }
-}
+};
 
 const signin = async (req, res) => {
-  let { email, password, name, last_name, phone, address, img } = req.body
-  password = bcrypt.hashSync(password)
-
-  response = await TransportModel.signin({
-    email,
-    password,
-    name,
-    last_name,
-    phone,
-    address,
-    img,
-  })
-  res.json(response)
-}
+  try {
+    let { email, password, name, last_name, phone, address, img } = req.body;
+    password = bcrypt.hashSync(password);
+    response = await TransportModel.signin({
+      email,
+      password,
+      name,
+      last_name,
+      phone,
+      address,
+      img,
+    });
+    res.json(response);
+  } catch (e) {
+    console.log('falla', error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
+  }
+};
 
 const list = async (req, res) => {
-  response = await TransportModel.list()
-  res.json(response)
-}
+  try {
+    response = await TransportModel.list();
+    res.json(response);
+  } catch (e) {
+    console.log('falla', error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
+  }
+};
 
 const getById = async (req, res) => {
-  const id = req.params.id
-  response = await TransportModel.getById(id)
-  res.json(response)
-}
+  try {
+    const id = req.params.id;
+    response = await TransportModel.getById(id);
+    res.json(response);
+  } catch (e) {
+    console.log('falla', error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
+  }
+};
 
 const remove = async (req, res) => {
-  const id = req.params.id
-  response = await TransportModel.remove(id)
-  res.json(response)
-}
+  try {
+    const id = req.params.id;
+    response = await TransportModel.remove(id);
+    res.json(response);
+  } catch (e) {
+    console.log('falla', error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
+  }
+};
 
 const update = async (req, res) => {
-  const id = req.params.id
-  response = await TransportModel.update(id)
-  res.json(response)
-}
+  try {
+    const id = req.params.id;
+    response = await TransportModel.update(id);
+    res.json(response);
+  } catch (e) {
+    console.log('falla', error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
+  }
+};
 
 const deleteByEmail = async (req, res) => {
-  const email = req.params.email
-  const response = await TransportModel.deleteByEmail(email)
-  res.json(response)
-}
+  try {
+    const email = req.params.email;
+    const response = await TransportModel.deleteByEmail(email);
+    res.json(response);
+  } catch (e) {
+    console.log('falla', error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
+  }
+};
 
 const updateData = async (req, res) => {
   try {
-    const { id } = req.params
-    const { name, last_name, phone, address } = req.body
+    const { id } = req.params;
+    const { name, last_name, phone, address } = req.body;
     const response = await TransportModel.updateData(
       name,
       last_name,
       phone,
       address,
       id
-    )
-    const transport = await TransportModel.getById(id)
-    const tokenPayload = getTokenBody(transport)
+    );
+    const transport = await TransportModel.getById(id);
+    const tokenPayload = getTokenBody(transport);
     response === 'error'
       ? res.send('Error al actualizar el registro en la base de datos')
-      : res.send(jwt.sign(tokenPayload, process.env.JWT_SECRET))
+      : res.send(jwt.sign(tokenPayload, process.env.JWT_SECRET));
   } catch (error) {
-    console.log('falla', error)
-    res.status(500).json({ message: 'Error al actualizar el usuario' })
+    console.log('falla', error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
   }
-}
+};
 
 const updatePassword = async (req, res) => {
   try {
-    const { id } = req.params
-    let { password } = req.body
-    console.log('llego aca', password, id)
-    password = bcrypt.hashSync(password)
-    const response = await TransportModel.updatePassword(password, id)
-    const transport = await TransportModel.getById(id)
-    const tokenPayload = getTokenBody(transport)
+    const { id } = req.params;
+    let { password } = req.body;
+    console.log('llego aca', password, id);
+    password = bcrypt.hashSync(password);
+    const response = await TransportModel.updatePassword(password, id);
+    const transport = await TransportModel.getById(id);
+    const tokenPayload = getTokenBody(transport);
     response === 'error'
       ? res.send('Error 333333 al actualizar el registro en la base de datos')
-      : res.send(jwt.sign(tokenPayload, process.env.JWT_SECRET))
+      : res.send(jwt.sign(tokenPayload, process.env.JWT_SECRET));
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Error al actualizar el usuario' })
+    console.log(error);
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
   }
-}
+};
 
 module.exports = {
   login,
@@ -128,4 +162,4 @@ module.exports = {
   deleteByEmail,
   updateData,
   updatePassword,
-}
+};
