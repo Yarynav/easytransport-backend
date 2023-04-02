14021593f3558ef1767cf16df1b10fba1e110cb5
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2023-04-02 09:38:52 -0400

    feat:creation of enpoint for the creation of shipments

--- src/controllers/ShippingController.js
+++ src/controllers/ShippingController.js
@@ -1,35 +1,35 @@
-const ShippingModel = require('../models/ShippingModel');
+const ShippingModel = require('../models/ShippingModel')
 
 const getByClientId = async (req, res) => {
   try {
-    const clientId = req.query.clientId;
-    const shippings = await ShippingModel.getByClientId(clientId);
-    res.json(shippings);
+    const clientId = req.query.clientId
+    const shippings = await ShippingModel.getByClientId(clientId)
+    res.json(shippings)
   } catch (e) {
-    res.status(500).json({ message: e + '' });
+    res.status(500).json({ message: e + '' })
   }
-};
+}
 
 const getById = async (req, res) => {
   try {
-    const shippingId = req.params.id;
-    const shipping = await ShippingModel.getById(shippingId);
-    res.json(shipping);
+    const shippingId = req.params.id
+    const shipping = await ShippingModel.getById(shippingId)
+    res.json(shipping)
   } catch (e) {
-    res.status(500).json({ message: e + '' });
+    res.status(500).json({ message: e + '' })
   }
-};
+}
 
 const update = async (req, res) => {
   try {
-    const shippingId = req.params.id;
-    const body = req.body;
-    const shipping = await ShippingModel.update(shippingId, body);
-    res.json(shipping);
+    const shippingId = req.params.id
+    const body = req.body
+    const shipping = await ShippingModel.update(shippingId, body)
+    res.json(shipping)
   } catch (e) {
-    res.status(500).json({ message: e + '' });
+    res.status(500).json({ message: e + '' })
   }
-};
+}
 
 const create = async (req, res) => {
   try {
@@ -37,49 +37,40 @@ const create = async (req, res) => {
       trip_id,
       transport_id,
       client_id,
+      description,
       origin_address,
       destiny_address,
-      date_retirement,
-      time_ini_retirement,
-      time_end_retirement,
-      date_delivery,
-      time_ini_delivery,
-      time_end_delivery,
-      type_load_shipping,
       cubic_meters_shipping,
       weight_shipping,
       long_load_shipping,
       wide_load_shipping,
       high_load_shipping,
-    } = req.body;
+    } = req.body
+
     const response = await ShippingModel.create({
       trip_id,
       transport_id,
       client_id,
+      description,
       origin_address,
       destiny_address,
-      date_retirement,
-      time_ini_retirement,
-      time_end_retirement,
-      date_delivery,
-      time_ini_delivery,
-      time_end_delivery,
-      type_load_shipping,
       cubic_meters_shipping,
       weight_shipping,
       long_load_shipping,
       wide_load_shipping,
       high_load_shipping,
-    });
-    res.json({ msj: 'Creacion exitosa' });
+    })
+    response === 'error'
+      ? res.json({ msj: 'error al crear registro en la bd' })
+      : res.json({ msj: 'Creacion exitosa' })
   } catch (e) {
-    res.status(500).json({ msj: e });
+    res.status(500).json({ msj: e })
   }
-};
+}
 
 module.exports = {
   getByClientId,
   update,
   getById,
   create,
-};
+}
--- src/routes/ShippingRoutes.js
+++ src/routes/ShippingRoutes.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const ShippingController = require('../controllers/ShippingController');
-const { loggerMiddleware } = require('../middlewares/LoggerMiddleware');
-const { loginMiddleware } = require('../middlewares/LoginMiddleware');
-const { shippingMiddleware } = require('../middlewares/ShippingMiddleware');
-const middlewares = [loggerMiddleware, loginMiddleware];
-const createMiddlewares = [...middlewares, shippingMiddleware];
+const express = require('express')
+const router = express.Router()
+const ShippingController = require('../controllers/ShippingController')
+const { loggerMiddleware } = require('../middlewares/LoggerMiddleware')
+const { loginMiddleware } = require('../middlewares/LoginMiddleware')
+const { shippingMiddleware } = require('../middlewares/ShippingMiddleware')
+const middlewares = [loggerMiddleware, loginMiddleware]
 
 /** @description Obtiene todos los viajes relacionados a un cliente  */
-router.get('/byClient', middlewares, ShippingController.getByClientId);
-router.put('/:id', middlewares, ShippingController.update);
-router.get('/:id', middlewares, ShippingController.getById);
-router.post('/', createMiddlewares, ShippingController.create);
+router.get('/byClient', middlewares, ShippingController.getByClientId)
+router.put('/:id', middlewares, ShippingController.update)
+router.get('/:id', middlewares, ShippingController.getById)
+router.post(
+  '/',
 
-module.exports = router;
+  shippingMiddleware,
+  ShippingController.create
+)
+
+module.exports = router
